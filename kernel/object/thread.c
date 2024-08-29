3f0dc480b3bc032159756ea7c641d78426fff1ab
yangzhongming <2839309325@qq.com>
2024-08-29 10:40:09 +0800

    finish

--- kernel/object/thread.c
+++ kernel/object/thread.c
@@ -224,7 +224,6 @@ void create_root_thread(void)
         /* Allocate the init thread */
         thread = obj_alloc(TYPE_THREAD, sizeof(*thread));
         BUG_ON(thread == NULL);
-
         for (int i = 0; i < meta.phnum; i++) {
                 unsigned int flags;
                 unsigned long offset, vaddr, filesz, memsz;
@@ -234,29 +233,58 @@ void create_root_thread(void)
                                 + ROOT_PHDR_OFF + i * ROOT_PHENT_SIZE
                                 + PHDR_FLAGS_OFF),
                        sizeof(data));
-                flags = (unsigned int)le32_to_cpu(*(u32 *)data);
+                flags = (unsigned int)le64_to_cpu(*(u32 *)data);
 
                 /* LAB 3 TODO BEGIN */
                 /* Get offset, vaddr, filesz, memsz from image*/
+                memcpy(data,
+                       (void *)((unsigned long)&binary_procmgr_bin_start
+                                + ROOT_PHDR_OFF + i * ROOT_PHENT_SIZE
+                                + PHDR_OFFSET_OFF),
+                       sizeof(data));
+                offset = (unsigned long)le64_to_cpu(*(u64 *)data);
+                memcpy(data,
+                       (void *)((unsigned long)&binary_procmgr_bin_start
+                                + ROOT_PHDR_OFF + i * ROOT_PHENT_SIZE
+                                + PHDR_VADDR_OFF),
+                       sizeof(data));
+                vaddr = (unsigned long)le64_to_cpu(*(u64 *)data);
+                memcpy(data,
+                       (void *)((unsigned long)&binary_procmgr_bin_start
+                                + ROOT_PHDR_OFF + i * ROOT_PHENT_SIZE
+                                + PHDR_FILESZ_OFF),
+                       sizeof(data));
+                filesz = (unsigned long)le64_to_cpu(*(u64 *)data);
+                memcpy(data,
+                       (void *)((unsigned long)&binary_procmgr_bin_start
+                                + ROOT_PHDR_OFF + i * ROOT_PHENT_SIZE
+                                + PHDR_MEMSZ_OFF),
+                       sizeof(data));
+                memsz = (unsigned long)le64_to_cpu(*(u64 *)data);
 
                 /* LAB 3 TODO END */
-
                 struct pmobject *segment_pmo;
                 /* LAB 3 TODO BEGIN */
-
+                ret=create_pmo(ROUND_UP(memsz,PAGE_SIZE),PMO_DATA,root_cap_group,0,&segment_pmo);
                 /* LAB 3 TODO END */
-
                 BUG_ON(ret < 0);
-
                 /* LAB 3 TODO BEGIN */
                 /* Copy elf file contents into memory*/
-
+                memset((void *)phys_to_virt(segment_pmo->start),0,segment_pmo->size);
+                memcpy((void *)phys_to_virt(segment_pmo->start),(void *)(((unsigned long)&binary_procmgr_bin_start)+ROOT_BIN_HDR_SIZE+offset),filesz);
                 /* LAB 3 TODO END */
-                
                 unsigned vmr_flags = 0;    
                 /* LAB 3 TODO BEGIN */
                 /* Set flags*/
-
+                if(flags&PHDR_FLAGS_R){
+                        vmr_flags|=VMR_READ;
+                }
+                if(flags&PHDR_FLAGS_W){
+                        vmr_flags|=VMR_WRITE;
+                }
+                if(flags&PHDR_FLAGS_X){
+                        vmr_flags|=VMR_EXEC;
+                }
                 /* LAB 3 TODO END */
 
                 ret = vmspace_map_range(init_vmspace,
@@ -267,6 +295,7 @@ void create_root_thread(void)
                 BUG_ON(ret < 0);
 
         }
+
         obj_put(init_vmspace);
 
         stack = ROOT_THREAD_STACK_BASE + ROOT_THREAD_STACK_SIZE;
